b3867a2de5482ae3066ab4bed7cb5399e6253643
Shogo Sakihama <goshalways@gmail.com>
2020-07-10 20:19:48 +0900

    defaultを作っておく

---
+++ wordpress/wp-content/themes/wp-original-theme/index.php
@@ -0,0 +1,22 @@
+<!-- <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet"> -->
+
+
+<!-- <link rel="stylesheet" href="../output.css" /> -->
+<?php get_header(); ?>
+
+<div class="container m-auto sm:invisible md:visible lg:invisible xl:visible">
+    <main class="main">
+
+    <section class="top">
+        <div class="">
+          <h1 class="lg:text-center">
+            音大生たちのかぎりない生き方を知る。
+          </h1>
+          <img src="<?php bloginfo('template_directory'); ?>/images/common/side-logo.png" alt="roooot">
+          <span class="">とは、音大生がつくる、<br>音大生のためのウェブマガジン。<br>さまざまな世界で活躍する音大出身者の<br>インタビューを通じて、音楽にとどまらない<br>キャリアや生き方を見つけるきっかけを発信します。</span>
+          <div class="float-right p-20px" >fffffffffffffffffffffffff</div>
+        </div>
+      </section>
+      
+    </main>
+  </div>
